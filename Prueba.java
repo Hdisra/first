ce09ce3ea28f04e4e7fb67509a11e0d55ad43798
Hdisra <hdisra@gmail.com>
2021-02-06 12:24:06 -0600

    Mi primer commit

---
+++ Prueba.java
@@ -0,0 +1,13 @@
+/**
+ * Prueba con Git en Java
+ * @author Israel Hernández Dorantes
+ * @version 1.0 Febrero 2021
+ */
+public class Prueba {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello world");
+    }
+
+}
